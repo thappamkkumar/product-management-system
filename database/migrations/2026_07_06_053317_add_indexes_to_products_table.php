d964264f9dd763c9729fa487fded6c36563643cd
MUKESH KUMAR <thappamkkumar@gmail.com>
2026-07-06 11:05:43 +0530

    Index add to product title, created_at, data_available and user role

---
+++ database/migrations/2026_07_06_053317_add_indexes_to_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->index('title');
+            $table->index('created_at');
+            $table->index('date_available');
+        });
+        
+        Schema::table('users', function (Blueprint $table) {
+            $table->index('role');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+        $table->dropIndex(['title']);
+        $table->dropIndex(['created_at']);
+        $table->dropIndex(['date_available']);
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropIndex(['role']);
+        });
+    }
+};
